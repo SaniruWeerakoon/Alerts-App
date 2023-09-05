01eceb7d33b23b5a2f69caf7f10694dbf664ae4f
SaniruWeerakoon <cb010868@students.apiit.lk>
2023-09-05 19:01:48 +0530

    checking

--- lib/pages/load.dart
+++ lib/pages/load.dart
@@ -1,4 +1,3 @@
-import 'dart:ui_web';
 
 import 'package:flutter/material.dart';
 
@@ -7,102 +6,107 @@ class Loading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Stack(
+    return const Stack(
       children: [
-       const  Image(
+        Image(
           image: AssetImage('assets/images/background.jpg'),
         ),
         Positioned(
-          width: 360,
-          height: 800,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-             const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                // crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  // Text("Hello World"),
-                  Icon(
-                    Icons.access_alarms_outlined,
-                    color: Color.fromARGB(199, 192, 9, 9),
-                    size: 60,
-                  ),
-                  Icon(
-                    Icons.airplanemode_active,
-                    color: Color.fromARGB(198, 30, 18, 99),
-                    size: 60,
-                  ),
-                  Icon(
-                    Icons.coffee_outlined,
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    size: 60,
-                  ),
-                ],
-              ),
-             const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                // crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  // Text("Hello World"),
-                  Icon(
-                    Icons.airplanemode_active,
-                    color: Color.fromARGB(198, 30, 18, 99),
-                    size: 60,
-                  ),
-                  Icon(
-                    Icons.access_alarms_outlined,
-                    color: Color.fromARGB(199, 192, 9, 9),
-                    size: 60,
-                  ),
+          width: 0,
+          height: 0,
+          // child: Column(
+          //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //   children: [
+          //    const Row(
+          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //       // crossAxisAlignment: CrossAxisAlignment.stretch,
+          //       children: [
+          //         // Text("Hello World"),
+          //         Icon(
+          //           Icons.access_alarms_outlined,
+          //           color: Color.fromARGB(199, 192, 9, 9),
+          //           size: 60,
+          //         ),
+          //         Icon(
+          //           Icons.airplanemode_active,
+          //           color: Color.fromARGB(198, 30, 18, 99),
+          //           size: 60,
+          //         ),
+          //         Icon(
+          //           Icons.coffee_outlined,
+          //           color: Color.fromARGB(255, 0, 0, 0),
+          //           size: 60,
+          //         ),
+          //       ],
+          //     ),
+          //    const Row(
+          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //       // crossAxisAlignment: CrossAxisAlignment.stretch,
+          //       children: [
+          //         // Text("Hello World"),
+          //         Icon(
+          //           Icons.airplanemode_active,
+          //           color: Color.fromARGB(198, 30, 18, 99),
+          //           size: 60,
+          //         ),
+          //         Icon(
+          //           Icons.access_alarms_outlined,
+          //           color: Color.fromARGB(199, 192, 9, 9),
+          //           size: 60,
+          //         ),
 
-                  Icon(
-                    Icons.coffee_outlined,
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    size: 60,
-                  ),
-                ],
-              ),
-             const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                // crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Icon(
-                    Icons.coffee_outlined,
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    size: 60,
-                  ), // Text("Hello World"),
+          //         Icon(
+          //           Icons.coffee_outlined,
+          //           color: Color.fromARGB(255, 0, 0, 0),
+          //           size: 60,
+          //         ),
+          //       ],
+          //     ),
+          //    const Row(
+          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //       // crossAxisAlignment: CrossAxisAlignment.stretch,
+          //       children: [
+          //         Icon(
+          //           Icons.coffee_outlined,
+          //           color: Color.fromARGB(255, 0, 0, 0),
+          //           size: 60,
+          //         ), // Text("Hello World"),
 
-                  Icon(
-                    Icons.airplanemode_active,
-                    color: Color.fromARGB(198, 30, 18, 99),
-                    size: 60,
-                  ),
-                  Icon(
-                    Icons.access_alarms_outlined,
-                    color: Color.fromARGB(199, 192, 9, 9),
-                    size: 60,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                 const SizedBox(
-                    width: 300,
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      debugPrint("nice press");
-                    },
-                    backgroundColor: Colors.blueAccent,
-                    mouseCursor: SystemMouseCursors.precise,
-                    child: 
-                    const Center(child:   Icon(Icons.add,color: Colors.black,size: 60,)),
-                  )
-                ],
-              )
-            ],
-          ),
+          //         Icon(
+          //           Icons.airplanemode_active,
+          //           color: Color.fromARGB(198, 30, 18, 99),
+          //           size: 60,
+          //         ),
+          //         Icon(
+          //           Icons.access_alarms_outlined,
+          //           color: Color.fromARGB(199, 192, 9, 9),
+          //           size: 60,
+          //         ),
+          //       ],
+          //     ),
+          //     Row(
+          //       children: [
+          //        const SizedBox(
+          //           width: 300,
+          //         ),
+          //         FloatingActionButton(
+          //           onPressed: () {
+          //             debugPrint("nice press");
+          //           },
+          //           backgroundColor: Colors.blueAccent,
+          //           mouseCursor: SystemMouseCursors.precise,
+          //           child:
+          //           const Center(child:   Icon(Icons.add,color: Colors.black,size: 60,)),
+          //         )
+          //       ],
+          //     )
+          //   ],
+
+          child: Column(children: [
+            Image(
+              image: AssetImage('assets/images/appicon.png'),
+            )
+          ]),
         ),
       ],
     );
